f4ea3d55427b9fcf0c2604f8987807741c76fd78
Irineu A. Silva <mmmirineusilva@gmail.com>
2025-03-24 02:50:42 -0300

    Implement job

--- composeApp/src/commonMain/kotlin/com/example/second/Second.kt
+++ composeApp/src/commonMain/kotlin/com/example/second/Second.kt
@@ -1,22 +1,41 @@
 package com.example.second
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 
 @Composable
 fun Second(
     component: SecondComponent,
-) = Box(
+) = Column(
     modifier = Modifier.fillMaxSize(),
-    contentAlignment = Alignment.Center
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.spacedBy(
+        space = 8.dp,
+        alignment = Alignment.CenterVertically
+    )
 ) {
-    val item by component.text.subscribeAsState()
+    val uiState by component.uiState.subscribeAsState()
 
-    Text(text = item)
+    Text(
+        text = uiState.text,
+        color = animateColorAsState(uiState.color).value
+    )
+
+    Button(
+        onClick = {
+            component.startColorAnimation()
+        },
+    ) {
+        Text(text = "Animate color")
+    }
 }
--- composeApp/src/commonMain/kotlin/com/example/second/SecondComponent.kt
+++ composeApp/src/commonMain/kotlin/com/example/second/SecondComponent.kt
@@ -1,16 +1,65 @@
 package com.example.second
 
+import androidx.compose.ui.graphics.Color
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.Value
+import com.arkivanov.decompose.value.update
+import com.arkivanov.essenty.lifecycle.doOnDestroy
+import kotlinx.coroutines.*
+import kotlin.random.Random
 
 interface SecondComponent {
-    val text: Value<String>
+    val uiState: Value<UiState>
+
+    fun startColorAnimation()
+
+    data class UiState(
+        val text: String,
+        val color: Color
+    )
 }
 
 class DefaultSecondComponent(
     context: ComponentContext,
     name: String
 ) : SecondComponent, ComponentContext by context {
-    override val text = MutableValue(initialValue = "Hello, $name!")
+
+    private val coroutine = CoroutineScope(Dispatchers.Default)
+    private var colorAnimationJob: Job? = null
+
+    private val _uiState = MutableValue(
+        initialValue = SecondComponent.UiState(
+            text = name,
+            color = Color.Black
+        )
+    )
+
+    override val uiState = _uiState
+
+    init {
+        lifecycle.doOnDestroy { coroutine.cancel() }
+    }
+
+    override fun startColorAnimation() {
+        colorAnimationJob = coroutine.launch {
+            while (isActive) {
+                delay(timeMillis = 800)
+                _uiState.update {
+                    it.copy(
+                        color = colorRandom()
+                    )
+                }
+            }
+        }
+    }
+
+    private fun colorRandom(): Color {
+        return Color(
+            red = Random.nextInt(until = 255),
+            green = Random.nextInt(until = 255),
+            blue = Random.nextInt(until = 255),
+            alpha = 255
+        )
+    }
 }
